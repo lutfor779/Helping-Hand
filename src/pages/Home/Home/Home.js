f63fac21c812176b804b4996202ba748be29059a
unknown <sahinur4841@gmail.com>
2022-01-01 18:35:51 +0600

    latest news added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../../Shared/Header/Header';
+import LatestNews from '../LatestNews/LatestNews';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <LatestNews />
         </div>
     );
 };
