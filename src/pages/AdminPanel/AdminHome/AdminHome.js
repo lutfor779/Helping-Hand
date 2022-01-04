466ea7fb8a8037bc2e358752f67f93ab244841e4
Md. Lutfor Rahman <86643223+lutfor779@users.noreply.github.com>
2022-01-04 18:32:06 +0600

    makeadmin modifed
    * donation partners added
    
    * donation page added
    
    * donation form justified
    
    * home page modified
    
    * recharct npm added
    
    * text folder deleted
    
    * makeadmin modifed
    
    Co-authored-by: ihrimon <ihrimon147@gmail.com>

--- src/pages/AdminPanel/AdminHome/AdminHome.js
+++ src/pages/AdminPanel/AdminHome/AdminHome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 import AdminNav from '../AdminNav/AdminNav';
 
 const AdminHome = () => {
@@ -7,6 +8,7 @@ const AdminHome = () => {
         <div>
             <AdminNav />
             <Outlet />
+            <Footer />
         </div>
     );
 };
--- src/pages/AdminPanel/AdminNav/AdminNav.js
+++ src/pages/AdminPanel/AdminNav/AdminNav.js
@@ -2,12 +2,65 @@ import React from 'react';
 import { Button, Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import useAuth from '../../../hooks/useAuth';
+import logo from '../../../images/logo1.png'
+import './AdminNav.css'
 
 const AdminNav = () => {
     const { user, logOut } = useAuth();
 
 
     return (
+            <Navbar bg="primary" variant="dark" className='navbar' className="py-3" expand={false}>
+                <Container>
+                    <LinkContainer to="/">
+                        <Navbar.Brand >
+                            <img src={logo} height="35" alt="Logo" />
+                            </Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                    <Navbar.Offcanvas
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel">Admin Panel</Offcanvas.Title>
+                        </Offcanvas.Header>
+
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <LinkContainer to="makeAdmin" >
+                                    <Nav.Link>Make Admin</Nav.Link>
+                                </LinkContainer>
+
+                                <LinkContainer to="addEvent" >
+                                    <Nav.Link>Add Events</Nav.Link>
+                                </LinkContainer>
+
+                                {
+                                    user.displayName && <Navbar.Text>
+                                        {user.displayName} &nbsp;
+                                    </Navbar.Text>
+                                }
+                                {
+                                    user.email ?
+                                        <Button
+                                            variant='danger'
+                                            onClick={logOut}>Logout</Button>
+                                        :
+                                        <LinkContainer to="/login">
+                                            <Button>Login</Button>
+                                        </LinkContainer>
+                                }
+
+                            </Nav>
+
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+
         <Navbar bg="primary" variant="dark" expand={false}>
             <Container fluid>
                 <LinkContainer to="/">
@@ -60,6 +113,7 @@ const AdminNav = () => {
                 </Navbar.Offcanvas>
             </Container>
         </Navbar>
+
     );
 };
 
