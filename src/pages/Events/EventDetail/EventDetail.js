8b08ff4e6b6f83f479d2cb11f12b21ea2cacfb8b
lutfor779 <mdlrrahman779@gmail.com>
2022-01-04 23:59:12 +0600

    chenge

--- src/pages/Events/EventDetail/EventDetail.js
+++ src/pages/Events/EventDetail/EventDetail.js
@@ -9,7 +9,6 @@ import Rating from "react-rating";
 import swal from "sweetalert";
 import axios from "axios";
 import useAuth from "../../../hooks/useAuth";
-import { Button } from 'react-bootstrap';
 
 const EventDetail = () => {
   const { id } = useParams();
@@ -19,11 +18,6 @@ const EventDetail = () => {
   const [rating, setRating] = React.useState();
   const [event, setEvent] = useState({});
   const [isJoin, setIsJoin] = useState([]);
-
-
-
-  const [isJoin, setIsJoin] = useState([]);
-  const [event, setEvent] = useState({}); //use redux for store data
 
   useEffect(() => {
     fetch(`https://serene-bastion-42312.herokuapp.com/events/${id}`)
@@ -62,13 +56,11 @@ const EventDetail = () => {
     fetch(`https://serene-bastion-42312.herokuapp.com/joinedEvents/${user?.email}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         const x = data?.events?.filter(y => y === event?.title)
         setIsJoin(x)
       });
   }, [user?.email, event?.title]);
 
-  console.log(isJoin)
 
   const onSubmit = (e, data) => {
     console.log(data);
@@ -102,8 +94,8 @@ const EventDetail = () => {
                 <img src={event.image} className="img-fluid" alt="" />
                 <h6 className="mt-3"> Event Date: {event.date}</h6>
 
-                {isJoin.length === 0 && <Button variant="success " onClick={handleJoining}>Join in this event as volunteer</Button>}
-                {isJoin.length !== 0 && <h3 >You Already joined</h3>}
+                {isJoin?.length !== 0 && <Button variant="success " onClick={handleJoining}>Join in this event as volunteer</Button>}
+                {isJoin?.length === 0 && <h3 >You Already joined</h3>}
 
 
 
